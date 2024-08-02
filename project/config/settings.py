1988655426059ecf706ff2c3fc96a65c70d8f224
anakarlasantana <santanadeanakarla@gmail.com>
2024-08-02 08:09:15 -0300

    feat: Ultimas mudanças.

--- project/config/settings.py
+++ project/config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SPECTACULAR_SETTINGS = {
